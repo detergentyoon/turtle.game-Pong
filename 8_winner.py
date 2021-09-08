507f06693ed48b86bd5bc9fdde710d69d4a371c6
detergentyoon <detergentyoon@gmail.com>
2021-09-08 22:04:38 +0900

    2021-09-08

---
+++ 8_winner.py
@@ -0,0 +1,148 @@
+import turtle
+import winsound
+
+wn = turtle.Screen() # 창 호출
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width = 800, height = 600)
+wn.tracer(0) # 윈도우 트레이서 : 기본적으로 하는 작업으로 실제로 창을 멈추게 하는 것
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid = 1, stretch_len = 1)
+ball.penup()
+ball.goto(0, 0)
+ball.dx = .2
+ball.dy = .2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0) # 이동 속도 x , 애니메이션 속도 o
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player    A : 0                 Player    B : 0", align="center", font=("ARCADECLASSIC", 24, "normal"))
+
+# Restart "r"
+restart = turtle.Turtle()
+restart.speed(0)
+restart.penup()
+restart.hideturtle()
+restart.color("whitesmoke")
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor() # ycor 메소드는 turtle module 에서 가져옴. y 좌표를 반환 
+    y += 25
+    paddle_a.sety(y) # sety 를 새로운 y 로 적용
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 25
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 25
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 25
+    paddle_b.sety(y)
+
+
+
+# Keyboard binding
+wn.listen() # 창에 키보드의 입력(keyboard input)을 수신함
+
+wn.onkeypress(paddle_a_up, "w") # 소문자 w 이므로 caps lock 이 걸려있는 경우 작동하지 않게됨, w 키를 입력하면 paddle_a_up 함수를 호출
+wn.onkeypress(paddle_a_down, "s")
+
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx) # 현재 공의 좌표 + 움직임
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290) # y의 현 좌표가 290일 때, 다시 290으로 설정하여 특정 유형의 문제발생을 방지
+        ball.dy *= -1 # 방향 반전
+        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player    A : {score_a}                 Player    B : {score_b}", align="center", font=("ARCADECLASSIC", 24, "normal"))
+        winsound.PlaySound("sounds/goal.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear() # 이전 스코어 화면에서 제거(텍스트 곂침 방지)
+        pen.write(f"Player    A : {score_a}                 Player    B : {score_b}", align="center", font=("ARCADECLASSIC", 24, "normal"))
+        winsound.PlaySound("sounds/goal.wav", winsound.SND_ASYNC)
+
+        
+    # Paddle & Ball collision
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 55 and ball.ycor() > paddle_a.ycor() - 55):
+        ball.setx(-340) # 패들면을 미끄러지는 현상 방지, setx를 할당해줌으로서 패들과 충돌 시 바로 튕겨나갈 수 있도록 함
+        ball.dx *= -1
+        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 55 and ball.ycor() > paddle_b.ycor() - 55):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+
+    # Winner display
+    if score_a == 3:
+        pen.clear()
+        ball.reset()
+        paddle_a.reset()
+        paddle_b.reset()
+        pen.goto(0, 50)
+        pen.write("Player    A    Win ! !", align="center", font=("ARCADECLASSIC", 50, "normal"))
+        winsound.PlaySound("sounds/winner.wav", winsound.SND_NOWAIT)
+        restart.goto(0, -50)
+        restart.write("regame press r", align="center", font=("press start k", 25, "normal"))
--- README.md
+++ README.md
@@ -1,28 +1,29 @@
-`turtle module` 은 많은 기능을 가진 `pygame`에 비해 기능성은 떨어지지만 간단한 게임 제작의 경우 이 방법이 더 쉽게 시작할 수 있습니다.
-창을 여는데 매우 편리하고 고전적인 구식 프로그램 게임에 잘 작동합니다.
+`turtle module`은 많은 기능을 가진 `pygame`에 비해 기능성은 떨어지지만 간단한 게임 제작의 경우 이 방법이 더 쉽게 시작할 수 있습니다.
+
+turtle은 그래픽 창을 여는데 매우 편리하고 고전적인 구식 프로그램에 잘 작동합니다.
+
 (이 프로그램은 초보자를 위해 작성되었으므로 실제로는 객체 지향 프로그래밍을 사용하지 않습니다.)
 
 # **2_ game_objects**
-### Paddle process
 ```python
 paddle_a = turtle.Turtle()
 ```
-`turtle` = `turtle` module 적용
-`Turtle` = `Turtle class`
+`turtle` = `turtle` module 명렁어를 적용합니다.  
+`Turtle` = `Turtle class` 를 의미합니다.
 
 <br>
 
 ```python
 paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
 ```
-너비와 높이가 5:1 비율
+너비와 높이가 5:1 비율을 이루도록 합니다.
 
 <br>
 
 ```python
 paddle_a.penup()
 ```
-`penup` : 설정값을 실제로 화면에 그려주는 역할
+`penup` : 설정한 값을 turtle이 실제로 화면에 그려주는 역할을 합니다.
 
 <br>
 
@@ -30,7 +31,8 @@ paddle_a.penup()
 ```python
 paddle_a.goto(-350, 0)
 ```
-![sd](info\screen.jpg)
+![sd](info\screen.jpg)  
+중앙이 좌표의 (0, 0)으로 적용되어 있습니다.
 
 <br>
 
@@ -41,5 +43,37 @@ paddle_a.goto(-350, 0)
         ball.setx(-340) # 공이 패들면에서 미끄러지는 현상 방지
         ball.dx *= -1
 ```
-`(ball.xcor() < -340 and ball.xcor() > -350)` : 패들 뒤에서 공이 튕기는 현상 방지
-`ball.setx(-340)` : 공이 패들과 접촉 시 패들의 면에서 미끄러지는 현상 방지, 이것은 공이 이동하던 좌표가 -340 보다 더 작아질 때 발동하는 if문 때문에 생기는 버그로, setx를 할당해줌으로서 공이 패들과 충돌 시 곧바로 튕겨나갈 수 있도록 해줌
+`(ball.xcor() < -340 and ball.xcor() > -350)` : 패들 뒷면과 화면 경계 사이에서 비정상적으로 공이 튕기며 득점 반영이 되지 않는 현상을 방지합니다.
+
+`ball.setx(-340)` : 공이 패들과 접촉 시 패들의 면에서 미끄러지는 현상을 방지합니다. 이것은 공이 이동하던 좌표가 -340 보다 더 작아질 때만 호출되도록 설정한 `if`문 때문에 생기는 버그로, `setx`를 할당해줌으로서 공이 패들과 충돌 시 곧바로 튕겨나갈 수 있도록 해줍니다.
+
+<br>
+
+# **8_ winner**
+```python
+# Winner display
+if score_a == 3:
+    pen.clear()
+    ball.reset()
+    paddle_a.reset()
+    paddle_b.reset()
+    pen.goto(0, 50)
+    pen.write("Player    A    Win ! !", align="center", font("ARCADECLASSIC", 50, "normal"))
+    winsound.PlaySound("sounds/winner.wav", winsound.SND_NOWAIT)
+    restart.goto(0, -50)
+    restart.write("regame press r", align="center", font=("pressstart k", 25, "normal")
+```
+3점(예시)을 먼저 달성하는 경우 승리 & 재시작 유무 화면을 띄웁니다.
+
+이 때 이전에 그려진 turtle 요소들을 화면에서 지우고, A 와 B의 점수판으로 사용되던 텍스트란 `pen`은 `goto` 를 통해 새로운 좌표로 옮기고, `write` 를 통해 텍스트 내용을 **수정**하기로 했습니다.
+
+그렇기 때문에 이전의 `pen`의 설정 내용들을 `clear()`를 통해 그렸던 요소들만 삭제 후 위치는 그대로 두었습니다.
+>`turtle.clear()` 이 turtle이 그렸던 모든 것을 삭제합니다. (turtle의 **위치는 유지**됩니다.)
+
+>`turtle.reset()` 이 turtle의 상태(예: 방향, 위치 등)를 수행한 `turtle clear()` 다음 재설정 합니다. (turtle의 **위치도 초기화**됩니다.)
+
+<br>
+
+반대로 `ball`과 `paddle`은 `pen`처럼 수정하여 재사용하지 않고 화면에서 완전히 사라지길 바랬기 때문에 `reset()`을 통해 turtle의 모든 수행을 초기화시켜 화면에서 완전히 지워버렸습니다.
+
+그래서 하나의 플레이어가 승리 점수에 도달하면 위 logic을 통해 승리 & 재시작 유무 화면으로 전환된 것 처럼 나타낼 수 있었습니다.
